ec49ea9dd8c929168bc4cbba81b81523b968bc2c
Your Name <you@example.com>
2023-01-16 15:13:30 +0530

    Use localhost to open the site

--- video_app/video_app/settings.py
+++ video_app/video_app/settings.py
@@ -27,8 +27,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 #DEBUG = True
 DEBUG = os.getenv('DEBUG')
 
-#ALLOWED_HOSTS = ['192.168.0.105','127.0.0.1', 'localhost','0.0.0.0']
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['192.168.0.105','127.0.0.1', 'localhost','0.0.0.0','django']
+#ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -121,8 +121,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'stream')
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
