6f83f25eedb304c3254c19abe76cfc371a472ec4
alisterubuntu <alisterubuntu@gmail.com>
2023-01-20 23:43:42 +0530

    Updating the code

--- video_app/video_app/settings.py
+++ video_app/video_app/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'fpfd*qb+!is4hf@l6c(0n*1v4syzidbwzfsm-^%c30x*&772wc'
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
